a861e9e147587a1dca758401fb96558e886232d3
Shuvankr Hazra <shuvo.s.ovuhs@gmail.com>
2024-02-02 10:38:56 +0600

    loop added

---
+++ loop.js
@@ -0,0 +1,18 @@
+const mobile = {
+    brand: "Samsung",
+    price: 25000,
+    color: "Black",
+    camera: "12 MP",
+    isNew: true,
+}
+
+for(const prop in mobile){
+    console.log(prop);
+    console.log(mobile[prop]);
+}
+
+const keys = Object.keys(mobile);
+console.log(keys);
+for(const key of keys){
+    console.log(key,":",mobile[key]);
+}
